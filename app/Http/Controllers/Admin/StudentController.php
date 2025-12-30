f51ad6896d9f549d503bda753999c8103d97e05b
masud <mrmasud151821@gmail.com>
2025-12-30 15:28:32 +0600

    Create Student Controller method updated

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -45,114 +45,78 @@ class StudentController extends Controller
     public function store(StudentAdmissionRequest $request)
     {
         $data = $request->validated();
-        // $uploadedImagePath = null;
+        $uploadedImagePath = null;
 
-        // try {
-        //     DB::beginTransaction();
-        //     if ($request->hasFile('photo')) {
-        //         $uploadedImagePath = $request->file('photo')->store('uploads/students', 'public');
-        //         $data['photo'] = $uploadedImagePath;
-        //     }
+        try {
+            DB::beginTransaction();
+            if ($request->hasFile('photo')) {
+                $uploadedImagePath = $request->file('photo')->store('uploads/students', 'public');
+                $data['photo'] = $uploadedImagePath;
+            }
 
-        //     if (empty($data['admission_no'])) {
-        //         $data['admission_no'] = 'ADM' . time();
-        //     }
+            if (empty($data['admission_no'])) {
+                $data['admission_no'] = 'ADM' . time();
+            }
 
-        //     $studentData = [
-        //         'admission_no'       => $data['admission_no'],
-        //         'first_name'         => $data['first_name'],
-        //         'last_name'          => $data['last_name'],
-        //         'dob'                => $data['dob'],
-        //         'blood_group'        => $data['blood_group'] ?? null,
-        //         'gender'             => $data['gender'],
-        //         'email'              => $data['email'] ?? null,
-        //         'father_name'        => $data['father_name'],
-        //         'mother_name'        => $data['mother_name'],
-        //         'father_occupation'  => $data['father_occupation'],
-        //         'mother_occupation'  => $data['mother_occupation'],
-        //         'nationality'        => $data['nationality'],
-        //         'guardian_phone'     => $data['guardian_phone'],
-        //         'class_id'           => $data['class_id'],
-        //         'section_id'         => $data['section_id'],
-        //         'admission_date'     => $data['admission_date'],
-        //         'academic_year'      => $data['academic_year'],
-        //         'previous_school'    => $data['previous_school'] ?? null,
-        //         'address' => $data['current_address'] ?? null,
-        //         'photo'              => $data['photo'] ?? null,
-        //         'status'             => $data['status'],
-        //     ];
+            $studentData = [ 
+                'admission_no'      => $data['admission_no'] ?? 'ADM' . time(),
+     
+                'first_name'        => $data['first_name'],
+                'last_name'         => $data['last_name'],
+                'dob'               => $data['dob'],
+                'blood_group'       => $data['blood_group'] ?? null,
+                'gender'            => $data['gender'],
+     
+                'email'             => $data['email'] ?? null,
+     
+                'father_name'       => $data['father_name'],
+                'mother_name'       => $data['mother_name'],
+                'father_occupation' => $data['father_occupation'] ?? null,
+                'mother_occupation' => $data['mother_occupation'] ?? null,
+     
+                'nationality'       => $data['nationality'],
+                'religion'          => $data['religion'] ?? null,
+     
+                'guardian_relation' => $data['guardian_relation'],
+                'guardian_phone'    => $data['guardian_phone'],
+     
+                'roll_no'           => $data['roll_no'] ?? null,
+                'class_id'          => $data['class_id'],
+                'section_id'        => $data['section_id'],
+                'admission_date'    => $data['admission_date'],
+                'academic_year'     => $data['academic_year'], 
+                'student_status'    => $data['student_status'] ?? 0,  
+                'status'            => $data['status'] ?? 'pending',  
+     
+                'previous_school'   => $data['previous_school'] ?? null,
+                'address'           => $data['address'] ?? null,
+                'photo'             => $data['photo'] ?? null,
+            ];
 
-        //     Student::create($studentData);
+            Student::create($studentData);
 
-        //     DB::commit();
+            DB::commit();
 
-        //     return redirect()
-        //         ->back()
-        //         ->with('success', 'Student admitted successfully!');
+            return redirect()
+                ->back()
+                ->with('success', 'Student admitted successfully!');
 
-        // }  catch (\Exception $e) {
-        //     DB::rollBack();
+        }  catch (\Exception $e) {
+            DB::rollBack();
 
-        //     if ($uploadedImagePath && Storage::disk('public')->exists($uploadedImagePath)) {
-        //     Storage::disk('public')->delete($uploadedImagePath);
-        //     }
+            if ($uploadedImagePath && Storage::disk('public')->exists($uploadedImagePath)) {
+            Storage::disk('public')->delete($uploadedImagePath);
+            }
 
-        //     Log::error('Student Admission Error', [
-        //         'message' => $e->getMessage(),
-        //         'file'    => $e->getFile(),
-        //         'line'    => $e->getLine(),
-        //         'trace'   => $e->getTraceAsString(),
-        //     ]);
+            Log::error('Student Admission Error', [
+                'message' => $e->getMessage(),
+                'file'    => $e->getFile(),
+                'line'    => $e->getLine(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
 
-        //     return back()->with('error', 'Failed to admit student');
-        // }
-
-        $studentData = Student::create([
-            // required & unique
-            'admission_no'      => $data['admission_no'] ?? 'ADM' . time(),
-
-            // basic info
-            'first_name'        => $data['first_name'],
-            'last_name'         => $data['last_name'],
-            'dob'               => $data['dob'],
-            'blood_group'       => $data['blood_group'] ?? null,
-            'gender'            => $data['gender'],
-
-            // contact
-            'email'             => $data['email'] ?? null,
-
-            // parents
-            'father_name'       => $data['father_name'],
-            'mother_name'       => $data['mother_name'],
-            'father_occupation' => $data['father_occupation'] ?? null,
-            'mother_occupation' => $data['mother_occupation'] ?? null,
-
-            // personal
-            'nationality'       => $data['nationality'],
-            'religion'          => $data['religion'] ?? null,
-
-            // guardian
-            'guardian_relation' => $data['guardian_relation'],
-            'guardian_phone'    => $data['guardian_phone'],
-
-            // academics
-            'roll_no'           => $data['roll_no'] ?? null,
-            'class_id'          => $data['class_id'],
-            'section_id'        => $data['section_id'],
-            'admission_date'    => $data['admission_date'],
-            'academic_year'     => $data['academic_year'],
-
-            // status fields
-            'student_status'    => $data['student_status'] ?? 0, // tinyint(1)
-            'status'            => $data['status'] ?? 'pending', // enum
-
-            // optional
-            'previous_school'   => $data['previous_school'] ?? null,
-            'address'           => $data['address'] ?? null,
-            'photo'             => $data['photo'] ?? null,
-        ]);
-
-        dd($studentData);
+            return back()->with('error', 'Failed to admit student');
+        }
     }
 
 
